7ecf7785f4648ab3e4bf4d5274bfa74dadf4537e
arellanon <nahuel.arellano@gmail.com>
2018-02-08 04:13:34 +0000

    Se modifico los bash

---
+++ PageRank.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+./hadoop-run.sh grafos/web-BerkStan.txt
+#./hadoop-run.sh grafos/web-Google.txt
+#./hadoop-run.sh grafos/web-NotreDame.txt
+#./hadoop-run.sh grafos/web-Stanford.txt
--- hadoop-run.sh
+++ hadoop-run.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 times=1
-input=nodes
+input=$1
+input_file=$(basename "$input")
+
 #Se genera archivo temporal de values
-rm tmp_values
+rm tmp_values log_$input_file
 touch tmp_values
-hadoop fs -rm tmp_values
+hadoop fs -rm tmp_values resultado_$input_file
 hadoop fs -put tmp_values
 
 for i in `seq 1 $times`
 do
-	echo $i - $input
+	echo $i - $input_file
 	hadoop fs -rm -r tmp tmp2
 
-    timeIncio=$(date +%s)
-	echo $i - inicio: $(date) >> log.txt
+	timeIncio=$(date +%s)
+	echo $i - inicio: $(date) >> log_$input_file
 
 #	hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.8.2.jar \
-    hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.reduce.tasks=1 \
-        -file src \
+	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.reduce.tasks=1 \
+        	-file src \
 		-mapper "src/sortMapper.py" \
 		-reducer "src/sortReducer.py" \
 		-input tmp_values \
@@ -25,19 +27,20 @@ do
 		-output tmp
 
 #	hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.8.2.jar \
-    hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.reduce.tasks=1 \
-        -file src \
-        -mapper "src/prMapper.py" \
-        -reducer "src/prReducer.py" \
+    	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.reduce.tasks=1 \
+        	-file src \
+        	-mapper "src/prMapper.py" \
+        	-reducer "src/prReducer.py" \
 		-input tmp/part-00000 \
 		-output tmp2
 
 	hadoop fs -rm -r tmp_values
 	hadoop fs -mv tmp2/part-00000 tmp_values
 
-    timeFin=$(date +%s)
-    duracion=$((($timeFin-$timeIncio)/(60*60*24)))
-	echo $i - fin:    $(date) - duracion: $duracion >> log.txt
-
+	timeFin=$(date +%s)
+	duracion=$((($timeFin-$timeIncio)))
+	min=$(($duracion/60))
+	seg=$(($duracion-(min*60)))
+	echo $i - fin:    $(date) - duracion: $min:$seg   >> log_$input_file
 done
-hadoop fs -mv tmp_values resultado_$input
+hadoop fs -mv tmp_values resultado_$input_file
