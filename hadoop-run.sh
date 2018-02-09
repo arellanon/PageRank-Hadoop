9f0fcf30c1c122a2a4641f55e6da4813772768c7
arellanon <nahuel.arellano@gmail.com>
2018-02-09 23:00:07 +0000

    Se modifico la configuración de mapper

--- hadoop-run.sh
+++ hadoop-run.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 input=$1
-nro_map=$2
+nro_work=$2
 times=$3
 input_file=$(basename "$input")
 
 #Se genera archivo temporal de values
 rm tmp_values
 touch tmp_values
-hadoop fs -rm tmp_values resultado-m$nro_map-$input_file
+hadoop fs -rm tmp_values resultado-m$nro_work-$input_file
 hadoop fs -put tmp_values
 echo ............................................... >> log_$input_file
-echo $input_file - mapper: $nro_map >> log_$input_file
+echo $input_file - worker: $nro_work >> log_$input_file
 echo ............................................... >> log_$input_file
 totTimeIncio=$(date +%s)
 for i in `seq 1 $times`
 do
-	echo interacion: $i
+	echo iteracion: $i
 	echo ............................................... >> log_$input_file
 	echo interacion: $i - $input_file >> log_$input_file 
 	echo ............................................... >> log_$input_file
@@ -24,7 +24,8 @@ do
 	echo $i - inicio: $(date) >> log_$input_file
 
 #	hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.8.2.jar \
-	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.map.tasks=$nro_map -D mapred.reduce.tasks=1 \
+#	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.map.tasks=$nro_map -D mapred.reduce.tasks=1 \
+	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.reduce.tasks=1 \
         	-file src \
 		-mapper "src/sortMapper.py" \
 		-reducer "src/sortReducer.py" \
@@ -33,7 +34,8 @@ do
 		-output tmp
 
 #	hadoop jar $HADOOP_HOME/share/hadoop/tools/lib/hadoop-streaming-2.8.2.jar \
-    	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.map.tasks=$nro_map -D mapred.reduce.tasks=1 \
+#    	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.map.tasks=$nro_map -D mapred.reduce.tasks=1 \
+	hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -D mapred.reduce.tasks=1 \
         	-file src \
         	-mapper "src/prMapper.py" \
         	-reducer "src/prReducer.py" \
@@ -54,5 +56,5 @@ totTimeFin=$(date +%s)
 totDuracion=$((($totTimeFin-$totTimeIncio)))
 totMin=$(($totDuracion/60))
 totSeg=$(($totDuracion-(totMin*60)))
-echo Con $nro_map mapper - duracion total: $totMin:$totSeg   >> log_$input_file
-hadoop fs -mv tmp_values resultado-m$nro_map-$input_file
+echo Con $nro_work worker - duracion total: $totMin:$totSeg   >> log_$input_file
+hadoop fs -mv tmp_values resultado-m$nro_work-$input_file
