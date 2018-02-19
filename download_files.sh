ee4062f254d2a37067707211c2aa5fbf5ade5985
nahuel <nahuel.arellano@gmail.com>
2018-02-19 19:33:39 -0300

    Se prepara descarga de archivo Baidu internal links

--- download_files.sh
+++ download_files.sh
@@ -10,6 +10,7 @@ cd ~/grafos
 
 #Descargamos archivos
 #wikipedia
+:'
 wget http://nrvis.com/download/data/web/web-wikipedia_link_it.zip 
 unzip web-wikipedia_link_it.zip
 rm readme.html
@@ -17,9 +18,7 @@ rm web-wikipedia_link_it.zip
 #quitamos encabezado
 cat web-wikipedia_link_it.edges | tail -n +2 >> web-wikipedia_link_it.txt
 rm web-wikipedia_link_it.edges
-hadoop fs -put web-wikipedia_link_it.txt grafos/
 
-:'
 #web-BerkStan.txt
 wget http://snap.stanford.edu/data/web-BerkStan.txt.gz
 gzip -d web-BerkStan.txt.gz
@@ -34,5 +33,13 @@ mv web-Google.txt web-Google-head.txt
 cat web-Google-head.txt | tail -n +5 >> web-Google.txt
 rm web-Google-head.txt
 '
+wget http://konect.uni-koblenz.de/downloads/tsv/zhishi-baidu-relatedpages.tar.bz2
+tar -xjvf zhishi-baidu-relatedpages.tar.bz2
+mv zhishi-hudong-relatedpages/out.zhishi-hudong-relatedpages .
+rm -r zhishi-hudong-relatedpages/
+rm zhishi-hudong-relatedpages.tar.bz2
+head out.zhishi-hudong-relatedpages | tail -n +3 >> out.zhishi-hudong-relatedpages.txt
+rm out.zhishi-hudong-relatedpages
+
 #Subimos todos los archivos del folder ~/grafos/
 hadoop fs -put ~/grafos/* grafos/
